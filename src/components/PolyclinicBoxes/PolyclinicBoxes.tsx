d6e174ec96b0a3b83a2441ebd58e78c7dca6c792
Nick <studioo1533@gmail.com>
2018-12-06 13:35:59 +0100

    Bugfix/polyclinic boxes (#24)
    * fixes
    
    * fix
    
    * update img
    
    * Update PolyclinicsBoxes component: fix layout wrapping

--- src/components/PolyclinicBoxes/PolyclinicBoxes.tsx
+++ src/components/PolyclinicBoxes/PolyclinicBoxes.tsx
@@ -4,22 +4,25 @@ export interface PolyclinicBoxesProps {}
 
 const PolyclinicBoxes = (props: PolyclinicBoxesProps) => {
   return (
-    <div className="polyclinicBoxes container">
-      <h3>Další polikliniky</h3>
-      <ul className="flexRow">
-        <li>
-          <a href="#"><img src="/assets/medicon/images/poliklinika.png" alt="poliklinika" /></a>
-        </li>
-        <li>
-          <a href="#"><img src="/assets/medicon/images/poliklinika.png" alt="poliklinika" /></a>
-        </li>
-        <li>
-          <a href="#"><img src="/assets/medicon/images/poliklinika.png" alt="poliklinika" /></a>
-        </li>
-        <li>
-          <a href="#"><img src="/assets/medicon/images/poliklinika.png" alt="poliklinika" /></a>
-        </li>
-      </ul>
+    <div className={'polyclinicBoxes'}>
+      <div className={'container'}>
+        <h3>Další polikliniky</h3>
+
+        <ul className={'grid'}>
+          <li>
+            <a href="#"><img src="/assets/medicon/images/poliklinika.png" alt="poliklinika" /></a>
+          </li>
+          <li>
+            <a href="#"><img src="/assets/medicon/images/poliklinika.png" alt="poliklinika" /></a>
+          </li>
+          <li>
+            <a href="#"><img src="/assets/medicon/images/poliklinika.png" alt="poliklinika" /></a>
+          </li>
+          <li>
+            <a href="#"><img src="/assets/medicon/images/poliklinika.png" alt="poliklinika" /></a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
