9af47df87c56228aab0f12e809ce5d9eec4597cf
Emilio Herrera <emilioherrerasoukup@gmail.com>
2018-12-12 13:19:12 +0100

    added crossroad form

--- src/components/CrossRoads/CrossRoads.tsx
+++ src/components/CrossRoads/CrossRoads.tsx
@@ -1,32 +1,32 @@
 import * as React from 'react';
 
-export interface CrossRoadsProps {}
+export interface CrossRoadsProps {
+  data: {
+    items: [
+      {
+        text: string;
+        image: object;
+      }
+    ];
+  };
+}
 
 const CrossRoads = (props: CrossRoadsProps) => {
+  const { items } = props.data;
+
   return (
     <section className={'crossRoads'}>
       <div className={'container'}>
         <div className={'crossRoads__wrapper'}>
-          <a className={'crossRoads__card'} href="">
-            <div className={'crossRoads__card__img'}>
-              <img src="./assets/medicon/images/polikliniky.png" />
-            </div>
-            <p>Hledám polikliniku </p>
-          </a>
-  
-          <a className={'crossRoads__card'} href="">
-            <div className={'crossRoads__card__img'}>
-              <img src="./assets/medicon/images/lekari.png" />
-            </div>
-            <p>Hledám lékaře</p>
-          </a>
-
-          <a className={'crossRoads__card'} href="">
-            <div className={'crossRoads__card__img'}>
-              <img src="./assets/medicon/images/rada.png" />
-            </div>
-            <p>Potřebuji poradit</p>
-          </a>
+          {items &&
+            items.map((item, index) => (
+              <a className={'crossRoads__card'} href="" key={index}>
+                <div className={'crossRoads__card__img'}>
+                  <img src="./assets/medicon/images/polikliniky.png" />
+                </div>
+                <p>{item.text}</p>
+              </a>
+            ))}
         </div>
       </div>
     </section>
