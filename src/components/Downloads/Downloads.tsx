88827de70670f3b3876779056d25367620870a20
Nick <studioo1533@gmail.com>
2018-12-17 16:12:28 +0100

    Update Downloads component (#60)
    * Update Downloads component: add form to backoffice
    
    * fix condition

--- src/components/Downloads/Downloads.tsx
+++ src/components/Downloads/Downloads.tsx
@@ -2,46 +2,42 @@ import * as React from 'react';
 import DividerCircles from '../DividerCircles';
 import SvgIcon from '@source/partials/SvgIcon';
 
-export interface DownloadsProps {}
+interface Downloads {
+  title: string;
+  url: string;
+}
+
+export interface DownloadsProps {
+  data: {
+    title: string;
+    description: string;
+    downloads: Downloads[];
+  };
+}
 
 const Downloads = (props: DownloadsProps) => {
+  const { title, description, downloads } = props.data;
+
   return (
     <div className={'container'}>
       <div className={'downloads'}>
-      
-        <h3>Dokumenty ke stažení</h3>
+        {title && <h3>{title}</h3>}
 
-        <p>Zde si můžete stáhnout potřebné dokumenty</p>
+        {description && <p>{description}</p>}
 
         <div className="grid downloads__list">
-          <div className="downloads__list__element">
-            <p>Jak podat stížnost</p>
-            <a className={'btn btn--blueBorder'}>
-              Stáhnout
-              <SvgIcon name={'download'} type={'lightBlue'} />
-            </a>
-          </div>
-          <div className="downloads__list__element">
-            <p>Lorem ipsum dolor</p>
-            <a className={'btn btn--blueBorder'}>
-              Stáhnout
-              <SvgIcon name={'download'} type={'lightBlue'} />
-            </a>
-          </div>
-          <div className="downloads__list__element">
-            <p>Lorem ipsum dolor</p>
-            <a className={'btn btn--blueBorder'}>
-              Stáhnout
-              <SvgIcon name={'download'} type={'lightBlue'} />
-            </a>
-          </div>
-          <div className="downloads__list__element">
-            <p>Lorem ipsum dolor</p>
-            <a className={'btn btn--blueBorder'}>
-              Stáhnout
-              <SvgIcon name={'download'} type={'lightBlue'} />
-            </a>
-          </div>
+          {downloads && downloads.map((item, i) => (
+            <div className={'downloads__list__element'} key={i}>
+              {item.title && <p>{item.title}</p>}
+              {item.url && 
+                <a href={item.url} className={'btn btn--blueBorder'}>
+                  Stáhnout
+                  <SvgIcon name={'download'} type={'lightBlue'} />
+                </a>
+              }
+            </div>
+          ))}
+
         </div>
       </div>
       <DividerCircles />
