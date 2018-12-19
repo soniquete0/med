557268b68b048d6781cfb90154c3f5cd7e4387f0
Nick <studioo1533@gmail.com>
2018-12-19 16:09:45 +0100

    Update ContactInfo component:  (#61)
    * Update ContactInfo component: add form to backoffice
    
    * Fix contactInfo: change names of variables
    
    * Update ContactInfo.tsx
    
    * Update contactinfo.json

--- src/components/ContactInfo/ContactInfo.tsx
+++ src/components/ContactInfo/ContactInfo.tsx
@@ -1,37 +1,45 @@
 import * as React from 'react';
+import ReactMarkdown from 'react-markdown';
 
-export interface ContactInfoProps {}
+export interface ContactInfoProps {
+  data: {
+    address: string;
+    phones: string;
+    emails: string;
+    additional: string;
+  };
+}
 
 const ContactInfo = (props: ContactInfoProps) => {
+  const { address, phones, emails, additional } = props.data;
+
   return (
     <div className={'contact-info'}>
       <div className={'container'}>
         <div className={'grid contact-info__grid'}>
-          
+        
           <div className={'grid contact-info__grid__element'}>
-            <img src="/assets/medicon/images/contact-info-1.png" alt="address"/>
+            <img src={'/assets/medicon/images/contact-info-1.png'} alt="address"/>
             <div>
-              <p>MEDICON a.s. Antala Staška 1670/80 140 00 Praha 4</p>
+              {address && <ReactMarkdown source={address} />}
             </div>
           </div>
           <div className={'grid contact-info__grid__element'}>
-            <img src="/assets/medicon/images/contact-info-2.png" alt="phone number"/>
+            <img src={'/assets/medicon/images/contact-info-2.png'} alt="phone number"/>
             <div>
-              <a href={'tel:+420 261 006 111'}>tel.: +420 261 006 111</a><br/>
-              <a href={'tel:+420 261 006 210'}>fax: +420 261 006 210</a>
+              {phones && <ReactMarkdown source={phones} />}
             </div>
           </div>
           <div className={'grid contact-info__grid__element'}>
-            <img src="/assets/medicon/images/contact-info-3.png" alt="e-mail"/>
+            <img src={'/assets/medicon/images/contact-info-3.png'} alt="e-mail"/>
             <div>
-              <a href={'mailto:info@mediconas.cz'}>info@mediconas.cz</a>
+              {emails && <ReactMarkdown source={emails} />}
             </div>
           </div>
           <div className={'grid contact-info__grid__element'}>
-            <img src="/assets/medicon/images/contact-info-4.png" alt="ICO and DIC"/>
+            <img src={'/assets/medicon/images/contact-info-4.png'} alt="ICO and DIC"/>
             <div>
-              <p>IČO: 284 63 293</p>
-              <p>DIČ: CZ 284 63 293</p>
+              {additional && <ReactMarkdown source={additional} />}
             </div>
           </div>
 
