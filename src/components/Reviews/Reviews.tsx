a16b89866be6a28c4e31b4148ca3a1ef05a16bef
Nick <studioo1533@gmail.com>
2018-12-19 16:11:38 +0100

    Update Review component (#54)
    * Update Review component: add form to backoffice
    
    * fix reviewElement
    
    * Update reviews.json
    
    * Update reviews.json
    
    * Update ReviewElement.tsx

--- src/components/Reviews/Reviews.tsx
+++ src/components/Reviews/Reviews.tsx
@@ -1,43 +1,34 @@
 import * as React from 'react';
 import ReviewElement from './components/ReviewElement';
 
-export interface ReviewsProps {}
-
-const data = {
-  items: [
-    {
-      img: '../../../assets/medicon/images/photo-1.jpg',
-      cite: '„Jedna z nejlepších poliklinik v Praze, od recepčních po lékaře.“',
-      starCount: 5,
-    },
-    {  
-      img: '../../../assets/medicon/images/photo-2.jpg',
-      cite: '„Jedna z nejlepších poliklinik v Praze, od recepčních po lékaře.“',
-      starCount: 5,
-    },
-    {
-      img: '../../../assets/medicon/images/photo-3.jpg',
-      cite: '„Jedna z nejlepších poliklinik v Praze, od recepčních po lékaře.“',
-      starCount: 5,
-    },
-  ],
-};
-
-class Reviews extends React.Component<any, any> {
-  public render() {
-    return (
-      <section className="reviews">
-        <h3>řekli o nás</h3>
-        <div className="container">
-          <div className="grid reviews__list">
-            {data.items.map((item, index) => (
-              <ReviewElement key={index} img={item.img} cite={item.cite} starCount={item.starCount} />
-            ))}
-          </div>
-        </div>
-      </section>
-    );
-  }
+interface Reviews {
+  cite: string;
+  image: LooseObject;
+  starCount: number;
 }
+
+export interface ReviewsProps {
+  data: {
+    title: string;
+    reviews: Reviews[];
+  };
+}
+
+const Reviews = (props: ReviewsProps) => {
+  const { title, reviews } = props.data;
+
+  return (
+    <section className="reviews">
+      {title && <h3>{title}</h3>}
+      <div className="container">
+        <div className="grid reviews__list">
+          {reviews && reviews.map((review, index) => (
+            <ReviewElement key={index} image={review.image} cite={review.cite} starCount={review.starCount} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default Reviews;
--- src/components/Reviews/components/ReviewElement.tsx
+++ src/components/Reviews/components/ReviewElement.tsx
@@ -1,25 +1,32 @@
 import * as React from 'react';
 import SvgIcon from '@source/partials/SvgIcon';
+import Media from '@source/partials/Media';
 
 export interface ReviewElementProps {
-  img: string;
   cite: string;
+  image: LooseObject;
   starCount: number;  
 }
 
 export default function ReviewElement(props: ReviewElementProps) {
+  const { image, cite, starCount } = props;
+
   var rows = [];
-  for (var i = 0; i < props.starCount; i++) {
+  for (let i = 0; i < starCount; i++) {
     rows.push(<SvgIcon name="star" key={i} />);
+  } // 5 stars by default
+  
+  if (starCount < 5) {
+    rows.slice(-(5 - starCount), starCount);
   }
 
   return (
-    <div className="reviews__list__element">
-      <img src={props.img} alt="photo" />
+    <div className={'reviews__list__element'}>
+      {image && <Media type={'image'} data={image}/>}
 
-      <div className="reviews__list__element__content">
-        <cite>{props.cite}</cite>
-        <div className="stars">{rows}</div>
+      <div className={'reviews__list__element__content'}>
+        {cite && <cite>{cite}</cite>}
+        {rows && <div className={'stars'}>{rows}</div>}
       </div>
     </div>
   );
