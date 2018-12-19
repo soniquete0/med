481dcbeaac7b21ef0e774c10e96f5ca0f9d88776
Nick <studioo1533@gmail.com>
2018-12-19 16:13:12 +0100

    Update Media and PolyclinicInfo component (#53)
    * Update Media component and PolyclinicInfo: add form to backoffice and add new type of Media
    
    * Fix bug
    
    * Fix polyclinicInfo component
    
    * Update PolyclinicInfo component: fix Media

--- src/components/PolyclinicInfo/PolyclinicInfo.tsx
+++ src/components/PolyclinicInfo/PolyclinicInfo.tsx
@@ -1,27 +1,37 @@
 import * as React from 'react';
+import Media from '@source/partials/Media';
 
-export interface PolyclinicInfoProps {}
+export interface ContactsProps {
+  data: {
+    geo: string;
+    phone: string;
+    transport: string;
+    transportImage: LooseObject;
+  };
+}
 
-const PolyclinicInfo = (props: PolyclinicInfoProps) => {
+const PolyclinicInfo = (props: ContactsProps) => {
+  const { geo, phone, transport, transportImage } = props.data;
+  
   return (
-    <section className="policlinicInfo">
-      <div className="policlinicInfo__item" style={{ backgroundImage: 'url(/assets/medicon/images/geoIcon.svg)' }}>
-        <p>
-          Antala Staška 1670/80
-          <br />
-          140 00 Praha 4
-        </p>
+    <div className={'policlinicInfo'}>
+
+      <div className={'policlinicInfo__item'}>
+        <img src={'/assets/medicon/images/geo.svg'} alt="address"/>
+        {geo && <p>{geo}</p>}
       </div>
-      <div className="policlinicInfo__item" style={{ backgroundImage: 'url(/assets/medicon/images/phoneIcon.svg)' }}>
-        <p>+420 261 006 111</p>
+      <div className={'policlinicInfo__item'}>
+        <img src={'/assets/medicon/images/phone.svg'} alt="phone nubmer"/>
+        {phone && <p>{phone}</p>}
       </div>
-      <div className="policlinicInfo__item" style={{ backgroundImage: 'url(/assets/medicon/images/metro.png)' }}>
-        <p>
-          Metro C <br />
-          stanice Budějovická
-        </p>
+      <div className={'policlinicInfo__item'}>
+        {(transportImage && <Media type={'image'} data={transportImage} />) || (
+          <img src={'/assets/medicon/images/metro.svg'} alt="metro"/>
+        )}
+        {transport && <p>{transport}</p>}
       </div>
-    </section>
+      
+    </div>
   );
 };
 
