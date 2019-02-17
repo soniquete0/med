ff5713b47ae24d65e83964e8436e26d42b36edf5
Nick <studioo1533@gmail.com>
2019-02-17 13:35:26 +0100

    Added field for url (#96)

--- src/components/Benefits/Benefits.tsx
+++ src/components/Benefits/Benefits.tsx
@@ -1,10 +1,12 @@
 import * as React from 'react';
 import Media from '@source/partials/Media';
 import List from '../List';
+import Link from '@source/partials/Link';
 
 interface Benefits {
   text: string;
   image: LooseObject;
+  url?: LooseObject;
 }
 
 export interface BenefitsProps {
@@ -22,7 +24,14 @@ const Benefits = (props: BenefitsProps) => {
           <div className={'container'}>
             <div className={'benefits__list grid'}>
               {data && data.map((benefit, index) => {
-                return (
+                return benefit.url ? (
+                  <Link key={index} url={benefit.url.url} className={'benefits__list__element grid'}>
+                    {benefit.image && benefit.image.filename && 
+                      <Media type={'image'} data={benefit.image} />}
+                      
+                    {benefit.text && <p>{benefit.text}</p>}
+                  </Link>
+                ) : (
                   <div key={index} className={'benefits__list__element grid'}>
                     {benefit.image && benefit.image.filename && 
                       <Media type={'image'} data={benefit.image} />}
