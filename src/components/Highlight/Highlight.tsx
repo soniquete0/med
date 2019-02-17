c8763d4280ffe6d34ec73b71d0ae860bebfe7ff5
Nick <studioo1533@gmail.com>
2019-02-17 13:36:11 +0100

    bugfix/122-component-highlight (#95)
    * fix highlight layout
    
    * Update highlight component

--- src/components/Highlight/Highlight.tsx
+++ src/components/Highlight/Highlight.tsx
@@ -1,33 +1,36 @@
 import * as React from 'react';
 import Button from '../../partials/Button';
+import ReactMarkdown from 'react-markdown';
 
 export interface HighlightProps {
   data: {
     text: string;
+    description: string;
+    urlTitle: string;
     url: LooseObject;
   };
   languageCode?: string;
 }
 
 const Highlight = (props: HighlightProps) => {
-  const { text, url } = props.data;
+  const { text, url, description, urlTitle } = props.data;
 
   return (
-    <div className="fullWidthContainer">
-      <section className={'highlight'}>
-        <div className="container">
-          <div className="flexRow flexAlign--center">
-            <div className={'highlight__info flexRow'}>
-              <img src="/assets/medicon/images/info.png" alt="info" />
-              {text && <p>{text}</p>}
-            </div>
-
-            <Button classes="btn--whiteBorder btn--fullWidth" url={url} >
-              vice info
-            </Button>
+    <div className={'highlight'}>
+      <div className={'container'}>
+        <div className={'highlight__content grid'}>
+          <img src="/assets/medicon/images/info.png" alt="info" />
+          <div>
+            {text && <p>{text}</p>}
+            {description && 
+              <ReactMarkdown className={'highlight__content--description'} source={description} />}
           </div>
+          
+          {url && <Button classes={'btn--whiteBorder'} url={url}>
+            {urlTitle && urlTitle || 'more info'}
+          </Button>}
         </div>
-      </section>
+      </div>
     </div>
   );
 };
