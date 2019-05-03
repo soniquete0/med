99ac9bdc21e23095f7c2d8c613581386e82da934
Nick <studioo1533@gmail.com>
2019-05-03 17:20:26 +0200

    Bugfix/183-u-lékařů-se-v-ordinačních-hodinách-nezobrazují-informace-které-jsou-doplněné-v-poznámkách-jako-například-u-mudr-andrea-skladaná (#149)
    * Update DoctorSchedule: fix view of schedule

--- src/components/DoctorSchedule/DoctorSchedule.tsx
+++ src/components/DoctorSchedule/DoctorSchedule.tsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 import moment from 'moment';
 import gql from 'graphql-tag';
 import { urlize } from 'urlize';
-import { cloneDeep } from 'lodash';
 import { Query } from 'react-apollo';
 
 import Link from '@source/partials/Link';
@@ -56,53 +55,8 @@ const getDayOfWeek = day => {
   }
 };
 
-const withinTime = (from, to, min, max) => {
-  from = moment(from, 'HH:mm');
-  to = moment(to, 'HH:mm');
-  min = moment(min, 'HH:mm');
-  max = moment(max, 'HH:mm');
-
-  if (from.isBetween(min, max) && to.isBetween(min, max)) {
-    return true;
-  } else { return false; }
-};
-
-const categorizeTimeSlot = (from, to) => {
-  if (withinTime(from, to, '00:00', '10:00')) {
-    return 'morning';
-  }
-  if (withinTime(from, to, '10:01', '12:00')) {
-    return 'noon';
-  }
-  if (withinTime(from, to, '12:01', '15:00')) {
-    return 'afternoon';
-  }
-  if (withinTime(from, to, '15:01', '00:00')) {
-    return 'lateAfternoon';
-  }
-
-  if (withinTime(from, to, '00:00', '12:01')) {
-    return 'morningAndNoon';
-  } 
-  if (withinTime(from, to, '00:00', '15:01')) {
-    return 'morningAndNoonAndAfternoon';
-  }
-  if (withinTime(from, to, '10:01', '15:01')) {
-    return 'noonAndAfternon';
-  }
-  if (withinTime(from, to, '10:01', '23:59')) {
-    return 'noonAndAfternonAndLateAfternoon';
-  }
-  if (withinTime(from, to, '12:01', '23:59')) {
-    return 'afternoonAndLateAfternoon';
-  } 
-  
-  return null;
-};
-
 const getWeekStructure = (week: LooseObject) => {
   let structuredWeek = [];
-  
   let weekDays = Object.keys(week.days);
   
   weekDays.forEach(day => {
@@ -111,104 +65,20 @@ const getWeekStructure = (week: LooseObject) => {
     weekDay.day = getDayOfWeek(day);
 
     if (week.days[day] && week.days[day].length > 0) {
-      week.days[day].forEach(time => {
-        let timeSlot = categorizeTimeSlot(time.from, time.to);
-
-        if (timeSlot) {
-          weekDay[categorizeTimeSlot(time.from, time.to) + 'Hours'] = {
-            description: time.note || '',
-            time: `${time.from} - ${time.to}`,
+      for (let i = 0; i < week.days[day].length; i++) {
+        week.days[day].map((time, index) => {
+          weekDay[index] = {
+            note: time.note || '',
+            time: `${time.from} - ${time.to}`, 
           };
-        }
-      });
+        });
+      }
     }
 
     structuredWeek.push(weekDay);
   });
 
-  return splitSchedule(structuredWeek);
-};
-
-const splitSchedule = (week: LooseObject) => {
-  let result = [];
-
-  week.forEach(day => {
-    let weekDay: LooseObject = {};
-
-    weekDay = cloneDeep(day);
-
-    // FIRST, SECOND COL
-    if (day.morningAndNoonHours) {
-      weekDay.morningHours = {
-        description: day.morningAndNoonHours.time.note || '',
-        time: day.morningAndNoonHours.time.slice(0, 8) + '10:00'
-      };
-      weekDay.noonHours = {
-        description: day.morningAndNoonHours.time.note || '',
-        time: '10:00 - ' + day.morningAndNoonHours.time.slice(-5)
-      };
-    }
-
-    // FIRST, SECOND, THIRD COL
-    if (day.morningAndNoonAndAfternoonHours) {
-      weekDay.morningHours = {
-        description: day.morningAndNoonAndAfternoonHours.time.note || '',
-        time: day.morningAndNoonAndAfternoonHours.time.slice(0, 8) + '10:00'
-      };
-      weekDay.noonHours = {
-        description: day.morningAndNoonAndAfternoonHours.time.note || '',
-        time: '10:00 - 12:00'
-      };
-      weekDay.afternoonHours = {
-        description: day.morningAndNoonAndAfternoonHours.time.note || '',
-        time: '12:00 - ' + day.morningAndNoonAndAfternoonHours.time.slice(-5)
-      };
-    }
-
-    // SECOND, THIRD COL
-    if (day.noonAndAfternonHours) {
-      weekDay.noonHours = {
-        description: day.noonAndAfternonHours.time.note || '',
-        time: day.noonAndAfternonHours.time.slice(0, 8) + '12:00'
-      };
-      weekDay.afternoonHours = {
-        description: day.noonAndAfternonHours.time.note || '',
-        time: '12:00 - ' + day.noonAndAfternonHours.time.slice(-5)
-      };
-    }
-
-    // SECOND, THIRD, FOURTH COL
-    if (day.noonAndAfternonAndLateAfternoonHours) {
-      weekDay.noonHours = {
-        description: day.noonAndAfternonAndLateAfternoonHours.time.note || '',
-        time: day.noonAndAfternonAndLateAfternoonHours.time.slice(0, 8) + '12:00'
-      };
-      weekDay.afternoonHours = {
-        description: day.noonAndAfternonAndLateAfternoonHours.time.note || '',
-        time: '12:00 - 15:00'
-      };
-      weekDay.lateAfternoonHours = {
-        description: day.noonAndAfternonAndLateAfternoonHours.time.note || '',
-        time: '15:00 - ' + day.noonAndAfternonAndLateAfternoonHours.time.slice(-5)
-      };
-    }
-
-    // THIRD, FOURHT COL
-    if (day.afternoonAndLateAfternoonHours) {
-      weekDay.afternoonHours = {
-        description: day.afternoonAndLateAfternoonHours.time.note || '',
-        time: day.afternoonAndLateAfternoonHours.time.slice(0, 8) + '15:00'
-      };
-      weekDay.lateAfternoonHours = {
-        description: day.afternoonAndLateAfternoonHours.time.note || '',
-        time: '15:00 - ' + day.afternoonAndLateAfternoonHours.time.slice(-5)
-      };
-    }
-
-    result.push(weekDay);
-  });
-  
-  return result;
+  return structuredWeek;
 };
  
 const getScheduleTitle = (regularity, oddWeekTitle, evenWeekTitle, regularWeekTitle) => {
@@ -242,13 +112,13 @@ const getAbsenceLink = (data, alternate) => {
 
 const DoctorSchedule = (props: DoctorScheduleProps) => {
   const { schedule, oddWeekTitle, evenWeekTitle, regularWeekTitle, absences } = props.data;
-
+  
   return (
     <section className={'container doctorScheduleSection'}>
       {schedule &&
         schedule.weeks &&
         schedule.weeks.map((week, i) => (
-          <div className="doctorSchedule" key={i}>
+          <div className="doctorSchedule" key={'1'}>
             <div className={'doctorSchedule__title'}>
               <h4>{getScheduleTitle(week.regularity, oddWeekTitle, evenWeekTitle, regularWeekTitle)}</h4>
             </div>
@@ -263,39 +133,32 @@ const DoctorSchedule = (props: DoctorScheduleProps) => {
                         <tr className={'mobileHeading'}>
                           <td colSpan={7}>{item.day}</td>
                         </tr>
-
                         <tr className={'normalRow'}>
                           <td>{item.day}</td>
                           <td>
-                            <p>{(item.morningHours && item.morningHours.time) || ' - '}</p>
-                            {item.morningHours && item.morningHours.description && (
-                              <p>{item.morningHours.description}</p>
-                            )}
+                            <p>{(item['0'] && item['0'].time) || ' - '}</p>
+                            {item['0'] && item['0'].note && <p>{item['0'].note}</p>}
                           </td>
                           <td className={'circleCell'}>
                             <DividerCircles />
                           </td>
                           <td>
-                            <p>{(item.noonHours && item.noonHours.time) || ' - '}</p>
-                            {item.noonHours && item.noonHours.description && <p>{item.noonHours.description}</p>}
+                            <p>{(item['1'] && item['1'].time) || ' - '}</p>
+                            {item['1'] && item['1'].note && <p>{item['1'].note}</p>}
                           </td>
                           <td className={'circleCell'}>
                             <DividerCircles />
                           </td>
                           <td>
-                            <p>{(item.afternoonHours && item.afternoonHours.time) || ' - '}</p>
-                            {item.afternoonHours && item.afternoonHours.description && (
-                              <p>{item.afternoonHours.description}</p>
-                            )}
+                            <p>{(item['2'] && item['2'].time) || ' - '}</p>
+                            {item['2'] && item['2'].note && <p>{item['2'].note}</p>}
                           </td>
                           <td className={'circleCell'}>
                             <DividerCircles />
                           </td>
                           <td>
-                            <p>{(item.lateAfternoonHours && item.lateAfternoonHours.time) || ' - '}</p>
-                            {item.lateAfternoonHours && item.lateAfternoonHours.description && (
-                              <p>{item.lateAfternoonHours.description}</p>
-                            )}
+                            <p>{(item['3'] && item['3'].time) || ' - '}</p>
+                            {item['3'] && item['3'].note && <p>{item['3'].note}</p>}
                           </td>
                         </tr>
                       </>
