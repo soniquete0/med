6f87620a68281a3cf6b0b968aedec704080ec278
studioo <studioo@example.org>
2019-06-14 13:26:04 +0200

    214-polyclinicinfo

--- build/components/PolyclinicInfo/PolyclinicInfo.js
+++ build/components/PolyclinicInfo/PolyclinicInfo.js
@@ -1,9 +1,23 @@
 "use strict";
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var React = require("react");
+var ReactMarkdown = require("react-markdown/with-html");
+var Link_1 = require("../../partials/Link");
 var Media_1 = require("../../partials/Media");
 var PolyclinicInfo = function (props) {
-    var _a = props.data, geo = _a.geo, phone = _a.phone, transport = _a.transport, transportImage = _a.transportImage, clinic = _a.clinic, clinicColor = _a.clinicColor;
+    var _a = props.data, geo = _a.geo, clinic = _a.clinic, transport = _a.transport, clinicColor = _a.clinicColor, geoUrl = _a.geoUrl, transportUrl = _a.transportUrl, phone = _a.phone, transportImage = _a.transportImage;
+    console.log(props.data);
     return (React.createElement("div", { className: 'policlinicInfo' },
         React.createElement("div", { className: "container" },
             React.createElement("div", { className: "row policlinicInfo__list" },
@@ -12,15 +26,31 @@ var PolyclinicInfo = function (props) {
                         React.createElement("img", { src: '/assets/medicon/images/geo.svg', alt: "address" }),
                         React.createElement("div", { className: 'policlinicInfo__item--content' },
                             React.createElement("p", { style: { display: 'block' } }, geo),
-                            clinic && React.createElement("p", { style: clinicColor ? { color: "" + clinicColor } : {} }, clinic)))),
+                            clinic && React.createElement("p", { style: clinicColor ? { color: "" + clinicColor } : {} }, clinic),
+                            geoUrl &&
+                                React.createElement(Link_1.default, __assign({}, geoUrl, { style: {
+                                        position: 'absolute',
+                                        width: '100%',
+                                        height: '100%',
+                                        top: 0,
+                                        left: 0
+                                    } }))))),
                 React.createElement("div", { className: "col-12 col-lg-4" },
                     React.createElement("div", { className: 'policlinicInfo__item' },
                         React.createElement("img", { src: '/assets/medicon/images/phone.svg', alt: "phone nubmer" }),
-                        React.createElement("p", { className: 'policlinicInfo__item--content' }, phone))),
+                        React.createElement("p", { className: 'policlinicInfo__item--content' }, phone && React.createElement(ReactMarkdown, { skipHtml: false, escapeHtml: false, source: phone })))),
                 React.createElement("div", { className: "col-12 col-lg-4" },
                     React.createElement("div", { className: 'policlinicInfo__item' },
                         transportImage && React.createElement(Media_1.default, { type: 'image', data: transportImage }),
-                        React.createElement("p", { className: 'policlinicInfo__item--content' }, transport)))))));
+                        React.createElement("p", { className: 'policlinicInfo__item--content' }, transport),
+                        transportUrl &&
+                            React.createElement(Link_1.default, __assign({}, transportUrl, { style: {
+                                    position: 'absolute',
+                                    width: '100%',
+                                    height: '100%',
+                                    top: 0,
+                                    left: 0
+                                } }))))))));
 };
 exports.default = PolyclinicInfo;
 //# sourceMappingURL=PolyclinicInfo.js.map
--- src/components/PolyclinicInfo/PolyclinicInfo.tsx
+++ src/components/PolyclinicInfo/PolyclinicInfo.tsx
@@ -1,49 +1,91 @@
 import * as React from 'react';
+import * as ReactMarkdown from 'react-markdown/with-html';
 
+import Link from '../../partials/Link';
 import Media from '../../partials/Media';
-import DividerCircles from '../DividerCircles';
 
 export interface ContactsProps {
   data: {
     geo: string;
     clinic?: string;
+    transport: string;
     clinicColor?: string;
+    geoUrl?: LooseObject;
+    transportUrl?: LooseObject;
     phone: string;
-    transport: string;  
     transportImage: LooseObject;
   };
 }
 
 const PolyclinicInfo = (props: ContactsProps) => {
-  const { geo, phone, transport, transportImage, clinic, clinicColor } = props.data;
+  const {
+    geo,
+    clinic,
+    transport,
+    clinicColor,
+    geoUrl,
+    transportUrl,
+    phone,
+    transportImage,
+  } = props.data;
+
+  console.log(props.data);
 
   return (
     <div className={'policlinicInfo'}>
       <div className="container">
-        {/* <DividerCircles /> */}
-
         <div className="row policlinicInfo__list">
+
           <div className="col-12 col-lg-4">
             <div className={'policlinicInfo__item'}>
               <img src={'/assets/medicon/images/geo.svg'} alt="address" />
               <div className={'policlinicInfo__item--content'}>
                 <p style={{ display: 'block' }}>{geo}</p>
                 {clinic && <p style={clinicColor ? {color: `${clinicColor}`} : {}}>{clinic}</p>}
+                {geoUrl && 
+                  <Link
+                    {...geoUrl} 
+                    style={{
+                      position: 'absolute',
+                      width: '100%',
+                      height: '100%',
+                      top: 0,
+                      left: 0
+                    }} 
+                  />}
               </div>
             </div>  
           </div>
           <div className="col-12 col-lg-4">
             <div className={'policlinicInfo__item'}>
               <img src={'/assets/medicon/images/phone.svg'} alt="phone nubmer" />
-              <p className={'policlinicInfo__item--content'}>{phone}</p>
+              <p className={'policlinicInfo__item--content'}>
+                {phone && <ReactMarkdown 
+                  skipHtml={false}
+                  escapeHtml={false}
+                  source={phone}
+                />}
+              </p>
             </div>
           </div>
           <div className="col-12 col-lg-4">
             <div className={'policlinicInfo__item'}>
               {transportImage && <Media type={'image'} data={transportImage} />}
               <p className={'policlinicInfo__item--content'}>{transport}</p>
+              {transportUrl && 
+                <Link 
+                  {...transportUrl} 
+                  style={{
+                    position: 'absolute',
+                    width: '100%',
+                    height: '100%',
+                    top: 0,
+                    left: 0
+                  }} 
+                />}
             </div>
           </div>
+
         </div>
       </div>
     </div>
